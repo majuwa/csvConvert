e537a56bb29f88bef1ced29309b0b22db4faba84
majuwa <maximilian.j.walter@gmail.com>
2018-05-01 12:02:30 +0200

    convert finished

--- convert.sh
+++ convert.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-gawk -vFPAT='([^,]*)|("[^"]+")' '{ for(i=1; i<NF; i++) if(substr($i,0,1)=="\"") printf "%s,",$i; else printf "\"%s\",",$i; if(NF) printf "\"%s\"",$NF; printf ORS}' $1 >> out.csv
- 
+#mkdir output_folder
+gawk -vFPAT='([^,]*)|("[^"]+")' '{ for(i=1; i<NF; i++) if(substr($i,0,1)=="\"") printf "%s,",$i; else printf "\"%s\",",$i; if(NF) printf "\"%s\"",$NF; printf ORS}' $1 |
+sed -E '1s/,""//g' |
+sed -E '2s/"",//g' |
+sed -E '2s/""//' |
+sed -z 's/\n/\n\n/g' > out.csv 
+
+
